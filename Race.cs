fd7973e4f57fb1071f1e60bceb99b1008ea0d89a
Vitaliy <kornilchenko@gmail.com>
2022-01-08 17:10:28 +0200

    Initial commit

---
+++ Race.cs
@@ -0,0 +1,64 @@
+﻿/*Разработать игру "Автомобильные гонки" с использованием делегатов.
+1. В игре использовать несколько типов автомобилей: спортивные, легковые, 
+грузовые и автобусы.
+2. Реализовать игру «Гонки». Принцип игры: Автомобили двигаются от 
+старта к финишу со скоростями, которые изменяются в установленных пределах 
+случайным образом. Победителем считается автомобиль, пришедший к финишу 
+первым
+Рекомендации по выполнению работы
+1. Разработать абстрактный класс «автомобиль» (класс Car). Собрать в нем 
+все общие поля, свойства (например, скорость) методы (например, ехать).
+2. Разработать классы автомобилей с конкретной реализацией конструкторов 
+и методов, свойств. В классы автомобилей добавить необходимые события 
+(например, финиш).
+3. Класс игры должен производить запуск соревнований автомобилей, 
+выводить сообщения о текущем положении автомобилей, выводить сообщение об 
+автомобиле, победившем в гонках. Создать делегаты, обеспечивающие вызов 
+методов из классов автомобилей (например, выйти на старт, начать гонку). 
+4. Игра заканчивается, когда какой-то из автомобилей проехал определенное 
+расстояние (старт в положении 0, финиш в положении 100). Уведомление об
+окончании гонки (прибытии какого-либо автомобиля на финиш) реализовать с 
+помощью событий.*/
+using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.Threading;
+
+namespace Gonki
+{
+    class Race
+    {
+        static void Main(string[] args)
+        {
+            SetWindowSize(120, 35);//размеры консоли
+            Track track = new Track(200); //создаем трассу 
+            List<RaceParticipant> participants = new List<RaceParticipant> { new RaceParticipant (Avto.TypeAvto.SPORTS, "Alfa Romeo GT" ),
+                                                                             new RaceParticipant (Avto.TypeAvto.SPORTS, "Audi RS 6" ),
+                                                                             new RaceParticipant (Avto.TypeAvto.CARS, "ZAZ 968" ),
+                                                                             new RaceParticipant (Avto.TypeAvto.BUSES, "YUTONG ZK6938HB9" ),
+                                                                             new RaceParticipant (Avto.TypeAvto.TRUCKS, "КАМАЗ-43509" )};
+            foreach(RaceParticipant item in participants)
+            {
+                track.start_race += item.statusAvto; //подписка на событие старт
+                item.FinishAvto += track.results_table; //подписка на событие финиш
+                item.Go(track.lengthTrack, 0); //выводим участников на старт и показываем информацию о них
+            }
+
+            track.Start();
+            int speed = 0, n= 0;
+            while(n!= participants.Count)
+            {
+                n = 0;//сбрасываем счетчик финишировавших
+                Thread.Sleep(1000); //задержка в 1сек
+                foreach (RaceParticipant item in participants) //перебираем всех участников
+                {
+                    speed = track.ChangeSpeed((int)item.my_distance); //определяем скорость на данном участке пути
+                    item.Go(track.lengthTrack, speed); //передаем скорость участнику
+                    if (item.finish) n++; //проверяем не все ли финишировали
+                }
+            }
+            SetCursorPosition(0, 25);
+            WriteLine("Гонка закончилась! Спасибо за внимание!");
+        }
+    }
+}    
---
+++ Track_avto.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace Gonki
+{
+    public class Track //трасса
+    {
+        public delegate void start_of_the_race(bool start= false); //делeгат (старт гонок)
+        public event start_of_the_race start_race; //определение события старт гонок
+        static Random rand = new Random();
+        public int lengthTrack { get; private set; } //протяженность трассы
+        int winner = 1; //порядок финишированных
+        public Track(int length) { lengthTrack = length; } //конструктор
+        public Track() : this(100) { } //конструктор по умолчанию
+        public void Start () //старт гонок
+        {
+            int n = 5; //отчет времени до старта
+            SetCursorPosition(1, 1);
+            Write("Старт через:");
+            while(n>=0)
+            {
+                if (n == 0)
+                {
+                    SetCursorPosition(1, 1);
+                    Write("                  "); //затираем старую стоку
+                    start_race?.Invoke(true); //создаем событие старт
+                }
+                else
+                {
+                    SetCursorPosition(14, 1);
+                    Write(n);
+                    Thread.Sleep(1000); //задержка в 1сек
+                }
+                n--;
+            }
+        }
+        public int ChangeSpeed(int distance) //скорость для данного участка дороги
+        {
+            int temp = (int)(distance * 1.0/ lengthTrack * 100); //процентное соотношение пройденого пути
+            if (temp < 25)
+                return rand.Next(80, 120);
+            else if (temp < 50)
+                return rand.Next(130, 220);
+            else if (temp < 75)
+                return rand.Next(180, 255);
+            else if (temp < 100)
+                return rand.Next(90, 150);
+            else //если достиг финиша
+                return 0;
+        }
+        public void results_table(string brand, int num)
+        {
+            SetCursorPosition(1, winner );
+            Write(winner + " место занял автомобиль " + brand + " под номером " + num);
+            winner++;
+        }
+    }
+    public abstract class Avto
+    {
+        public enum TypeAvto
+        {
+            SPORTS, //спортивные
+            CARS, //легковые
+            TRUCKS, //грузовики
+            BUSES //автобусы
+        };
+        public TypeAvto type_avto { get; set; } //тип авто
+        public string car_brand { get; set; } //марка авто
+        public float speed { get; set; } //скорость авто
+        public float my_distance { get; set; } = 0;
+        public abstract void Go(int distance , int speed); //абстрактный метод ехать
+        protected Avto(TypeAvto type, string brand)
+        { 
+            type_avto = type;
+            car_brand = brand;
+        }
+    }
+    public class RaceParticipant : Avto //участник гонки
+    {
+        public delegate void finish_avto(string str, int num); //делегат (финиш для объекта) 
+        public event finish_avto FinishAvto;// событие на финиш
+        public int num { get; set; } = 0; //номер участника гонки
+        static int s_num = 1; //переменная для присвоения номера участника
+        public bool finish = false; //финишировал ли
+       
+        public RaceParticipant(TypeAvto type, string brand):base(type, brand) //конструктор
+        {
+            num = s_num++;
+        }
+        public override void Go(int distance, int speed)
+        {
+            if (finish == true)//если финишировал выходим
+                return;
+            if (my_distance >= distance)
+            {
+                finish = true; //не даст еще раз создать событие 
+                FinishAvto?.Invoke(car_brand, num); //событие 
+                SetCursorPosition(9, 3 * num + 6);
+                Write("Участник гонок под номером " + num + " на автомобиле " + car_brand + ". Скорость " + speed + "км/ч. Пройденный путь " + distance + "км.    ");
+                statusAvto();
+                show_the_way(100);
+            }
+            else
+            {
+                my_distance += (float)(speed * 0.05);
+                SetCursorPosition(9, 3 * num + 6);
+                Write("Участник гонок под номером " + num + " на автомобиле " + car_brand + ". Скорость " + speed + "км/ч. Пройденный путь " + (int)(my_distance) + "км.");
+                show_the_way((int)(my_distance / distance * 100));
+            } 
+        }
+        private void show_the_way(int distance)
+        {
+            int i = 0;
+            SetCursorPosition(0, 3 * num + 7);
+            while (i++<= distance+6) //затираем прошлую позицию машины
+            { Write(" "); }
+            SetCursorPosition(distance, 3 * num + 7);// новая позиция авто
+            BackgroundColor = ConsoleColor.Red;
+            Write("  ");
+            ResetColor();
+        }
+        public void statusAvto(bool start = false)
+        {
+            if (start)
+            {
+                SetCursorPosition(1, 3 * num + 6);
+                Write("START ");
+            }
+            else
+            {
+                SetCursorPosition(1, 3 * num + 6);
+                Write("FINISH");
+            }
+        }
+    } 
+}
